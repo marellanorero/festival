235e0678c14612320fce47f1c085085f6ccc703a
marellanorero <marellanonorero@gmail.com>
2024-09-03 15:33:03 -0400

    performance web

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ function navegacionFija() {
     const sobreFestival = document.querySelector('.sobre-festival')
 
     window.addEventListener('scroll', function () {
-        if(sobreFestival.getBoundingClientRect().bottom < 1){
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
             header.classList.add('fixed')
         } else {
             header.classList.remove('fixed')
@@ -25,9 +25,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen galería'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+        `;
 
         //Event HAndler
         imagen.onclick = function () {
@@ -39,9 +42,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen galería'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
 
     //Generar modal
     const modal = document.createElement('DIV');
@@ -51,13 +57,13 @@ function mostrarImagen(i) {
 
     //btn cerrar modal
     const cerrarModalBtn = document.createElement('BUTTON');
-    cerrarModalBtn.textContent ='X';
+    cerrarModalBtn.textContent = 'X';
     cerrarModalBtn.classList.add('btn-cerrar');
     cerrarModalBtn.onclick = cerrarModal;
 
     modal.appendChild(imagen);
     modal.appendChild(cerrarModalBtn);
-    
+
     //Agregar al HTML
     const body = document.querySelector('body');
     body.classList.add('overflow-hidden')
@@ -75,41 +81,41 @@ function cerrarModal() {
     }, 500)
 }
 
-function resaltarEnlace(){
-    document.addEventListener('scroll', function(){
+function resaltarEnlace() {
+    document.addEventListener('scroll', function () {
         const sections = document.querySelectorAll('section')
         const navLinks = document.querySelectorAll('.navegacion-principal a')
 
         let actual = '';
-        sections.forEach( section => {
+        sections.forEach(section => {
 
             const sectionTop = section.offsetTop
             const sectionHeight = section.clientHeight
 
-            if(window.scrollY >= (sectionTop - sectionHeight / 3)) {
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
                 actual = section.id
             }
         })
 
         navLinks.forEach(link => {
             link.classList.remove('active')
-            if(link.getAttribute('href') === '#'+ actual){
+            if (link.getAttribute('href') === '#' + actual) {
                 link.classList.add('active')
-            }  
+            }
         })
     })
 }
 
-function scrollNav(){
-    const navLinks =document.querySelectorAll('.navegacion-principal a')
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
 
-    navLinks.forEach( link => {
+    navLinks.forEach(link => {
         link.addEventListener('click', e => {
             e.preventDefault();
             const sectionScroll = e.target.getAttribute('href')
             const section = document.querySelector(sectionScroll)
 
-            section.scrollIntoView({ behavior: 'smooth'} )
+            section.scrollIntoView({ behavior: 'smooth' })
         })
     })
 }
